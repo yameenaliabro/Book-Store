25ceefb57716242859973c320035cb6190453455
yameenaliabro <muhammadyameen5432@gmail.com>
2023-08-21 00:44:44 +0500

    add the ontext the incremnent and decrmenet quantity and the Cart UI implement increment and decrment quantity

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,5 +1,5 @@
 import { IProduct } from '@src/types';
-import React, { ReactNode, createContext, useContext, useState } from 'react';
+import React, { ReactNode, createContext, useCallback, useState, useMemo } from 'react';
 
 interface CartItem {
     product: IProduct;
@@ -10,6 +10,9 @@ interface CartContextType {
     cart: CartItem[];
     addToCart: (product: IProduct) => void;
     removeFromCart: (productId: string) => void;
+    incrementQuantity: (producId: string) => void,
+    decrementQuantity: (producId: string) => void,
+
 }
 
 export const CartContext = createContext<CartContextType | undefined>(undefined);
@@ -17,7 +20,22 @@ export const CartContext = createContext<CartContextType | undefined>(undefined)
 export const CartProvider = ({ children }: { children: ReactNode }) => {
     const [cart, setCart] = useState<CartItem[]>([]);
 
-    const addToCart = (product: IProduct) => {
+    const incrementQuantity = useCallback((productId: string) => {
+        setCart(preevcart =>
+            preevcart.map(item =>
+                item.product._id === productId ? { ...item, quantity: item.quantity + 1 } : item
+            )
+        )
+    }, [])
+
+    const decrementQuantity = useCallback((productId: string) => {
+        setCart(prevcart =>
+            prevcart.map(item =>
+                item.product._id === productId ? { ...item, quantity: item.quantity - 1 } : item
+            ))
+    }, [])
+
+    const addToCart = useCallback((product: IProduct) => {
         const existingItem = cart.find(item => item.product._id === product._id);
         if (existingItem) {
             setCart(prevCart =>
@@ -28,17 +46,25 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
         } else {
             setCart(prevCart => [...prevCart, { product, quantity: 1 }]);
         }
-    };
+    }, [cart]);
 
-    const removeFromCart = (productId: string) => {
+    const removeFromCart = useCallback((productId: string) => {
         setCart(prevCart => prevCart.filter(item => item.product._id !== productId));
-    };
+    }, []);
 
-    const contextValue: CartContextType = {
+    const contextValue: CartContextType = useMemo(() => ({
         cart,
         addToCart,
         removeFromCart,
-    };
+        decrementQuantity,
+        incrementQuantity
+    }), [
+        cart,
+        removeFromCart,
+        addToCart,
+        decrementQuantity,
+        incrementQuantity
+    ])
 
     return <CartContext.Provider value={contextValue}>{children}</CartContext.Provider>;
 };
--- src/pages/dashboard/cart/index.tsx
+++ src/pages/dashboard/cart/index.tsx
@@ -1,36 +1,57 @@
 import React from 'react';
-import { List, Image, Typography, Button, Popconfirm } from 'antd';
-import { DeleteOutlined, ShoppingCartOutlined } from '@ant-design/icons';
+import { List, Image, Typography, Button, Popconfirm, Input } from 'antd';
+import { DeleteOutlined, MinusCircleTwoTone, MinusOutlined, PlusOutlined, ShoppingCartOutlined } from '@ant-design/icons';
 import { useCart } from '@src/hooks/useCart';
 
-const CartUI: React.FC = () => {
-    const { cart: cartItems, removeFromCart } = useCart()
-
+const CartUI = () => {
+    const { cart: cartItems, removeFromCart, decrementQuantity, incrementQuantity } = useCart()
     return (
-        <List
-            itemLayout="vertical"
-            dataSource={cartItems}
-            renderItem={item => (
-                <List.Item
-                    key={item.product._id}
-                    extra={<Image width={100} src={item.product.image} alt={item.product.title} />}
-                >
-                    <List.Item.Meta
-                        title={item.product.title}
-                        description={`Price: $${item.product.purchaseprice.toFixed(2)}`}
-                    />
-                    <Typography.Text>Quantity: {item.quantity}</Typography.Text>
-                    <Popconfirm
-                        title="are you sure you want to delete this product!"
-                        okText="ok"
-                        cancelText="cancel"
-                        onConfirm={() => removeFromCart(item.product._id)}
+        <div>
+            <List
+                itemLayout="vertical"
+                className='m-20'
+                dataSource={cartItems}
+                renderItem={item => (
+                    <List.Item
+                        key={item.product._id}
+                        extra={<Image width={100} src={item.product.image} alt={item.product.title} />}
+                        className='flex flex-row'
                     >
-                        <Button type="primary" danger><DeleteOutlined /></Button>
-                    </Popconfirm>
-                </List.Item>
-            )}
-        />
+                        <Button
+                            type='primary'
+                            danger
+                            onClick={() => decrementQuantity(item.product._id)}
+                            disabled={item.quantity == 1}
+                            icon={<MinusOutlined />}
+                            className='mr-2'
+                        >
+                        </Button>
+                        <Typography.Text>{item.quantity}</Typography.Text>
+                        <Button
+                            type='primary'
+                            onClick={() => incrementQuantity(item.product._id)}
+                            className='ml-2'
+                            icon={<PlusOutlined />}
+                        >
+                        </Button>
+                        <List.Item.Meta
+                            title={item.product.title}
+                            description={`Price: $${item.product.purchaseprice.toFixed(2)}`}
+                        />
+                        <Typography.Text>Quantity: {item.quantity}</Typography.Text>
+                        <Popconfirm
+                            title="are you sure you want to delete this product!"
+                            okText="ok"
+                            cancelText="cancel"
+                            onConfirm={() => removeFromCart(item.product._id)}
+                            className='ml-3'
+                        >
+                            <Button type="primary" danger><DeleteOutlined /></Button>
+                        </Popconfirm>
+                    </List.Item>
+                )}
+            />
+        </div>
     );
 };
 
